564e5c1f82318aa472b3facac9e62241c93cc241
Jelle van der Zwaag <contact@tulipa.in>
2017-08-18 15:14:33 +0200

    added featured cities

--- views.py
+++ views.py
@@ -14,7 +14,7 @@ def index():
 @app.route('/<continent_id>/')
 def continent(continent_id):
 	countries = db.session.query(Country).filter(Country.continent_id==continent_id).all()
-	
+
 	return render_template('continent.html', countries=countries, continent_id=continent_id)
 
 @app.route('/<continent_id>/<country_id>/')
@@ -23,30 +23,28 @@ def country(continent_id,country_id):
 
 	cities = country.cities
 	timezones = country.timezones
-	
+
 	return render_template('country.html', country=country, cities=cities, timezones=timezones)
 
 @app.route('/<continent_id>/<country_id>/<city_id>/')
 def city(continent_id,country_id,city_id):
-	city = db.session.query(City).filter(City.name_id==city_id).first()
-	country = city.in_country
-	
-	poptext = population_text(city.name, city.population)
-	
-	return render_template('city.html', city=city, country=country, poptext=poptext)
+    city = db.session.query(City).filter(City.name_id==city_id).first()
+    country = city.in_country
+    poptext = population_text(city.name, city.population)
+    feat_cities = get_featured_cities()
+    #print(feat_cities)
+
+    return render_template('city.html', city=city, country=country, poptext=poptext, feat_cities=feat_cities)
 
 def population_text(city_name, population):
-	population = "{:,}".format(population).replace(',','.') 			
-		
+	population = "{:,}".format(population).replace(',','.')
+
 	texts = ['Het aantal inwoners van '+city_name+ ' bedraagt ' + population + '.',
 'Met ' + population + ' inwoners is ' + city_name + ' een behoorlijke plaats.', city_name + ' heeft ' + population + ' inwoners.']
-	
+
 	rndnum = len(city_name) % len(texts)
 	return texts[rndnum]
 
-
-
-
-
-
-	
+def get_featured_cities():
+    cities = db.session.query(City).filter(((City.name_id=='paris')&(City.country_code=='FR'))|((City.name_id=='london')&(City.country_code=='GB'))|((City.name_id=='sydney')&(City.country_code=='AU'))|((City.name_id=='new-york-city')&(City.country_code=='US'))|((City.name_id=='buenos-aires')&(City.country_code=='AR'))).all()
+    return cities
